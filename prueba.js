08527a9bd1a6b3474bbb9543dc1070b0c26de320
__Aimbot__ <manuelvalencia1801@gmail.com>
2025-04-30 15:22:44 -0500

    Tree

--- prueba.js
+++ prueba.js
@@ -1,1 +1,27 @@
-alert("a");
+const fs = require('fs');
+const path = require('path');
+
+
+function printTree(dirPath, level = 0) {
+    const items = fs.readdirSync(dirPath);
+
+    items.forEach(item => {
+        const fullPath = path.join(dirPath, item);
+        const stats = fs.statSync(fullPath);
+
+
+        if (stats.isDirectory()) {
+
+            console.log(' '.repeat(level * 2) + item);
+
+
+            if (level === 0) {
+                printTree(fullPath, level + 1);
+            }
+        }
+    });
+}
+
+
+const rootDir = path.join(__dirname);
+printTree(rootDir);
